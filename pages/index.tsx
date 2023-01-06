34423a2c3fb9fa121f85f9d4093ac27c34d47b2f
Chris Siku <chrissiku5@gmail.com>
2023-01-06 12:26:43 +0200

    Create home page mock-up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Home - Canal+ </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       Canal+ clone and redesign
+      {/* Header component */}
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+        </section>
+      </main>
+      {/* Modal */}
     </div>
   );
 };
