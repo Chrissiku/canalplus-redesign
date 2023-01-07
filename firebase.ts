134a09886d78cdf474c74cbb502e3d3989079860
Chris Siku <chrissiku5@gmail.com>
2023-01-07 16:50:05 +0200

    Create `firebase.ts` file and add configuration

---
+++ firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2Zpxl3aOmJoBxe-CA1HNqcIYxw7c3dc8",
+  authDomain: "canalplus-redesign.firebaseapp.com",
+  // databaseURL: 'https://next-firebase-stripe-39bf8-default-rtdb.firebaseio.com',
+  projectId: "canalplus-redesign",
+  storageBucket: "canalplus-redesign.appspot.com",
+  messagingSenderId: "696627738619",
+  appId: "1:696627738619:web:3ea32204f19bd179947d5e",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const auth = getAuth();
+
+export default app;
+export { auth, db };
